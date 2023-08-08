f9b9a1e8d2f07d15246d9b0e97f484f4db1b3fc9
Ebimary <igwemary220@gmail.com>
2023-08-08 12:42:37 -0700

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,32 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
+""" how many ssubscribed? """
+
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    """ Computes the numbers of subscribed in a subreddit
     Args:
-    subreddit (str): The name of the subreddit to query.
-    Returns:
-    int: The number of subscribers for the given subreddit. If the subreddit is invalid, returns 0.
+        subreddit(str): subreddit string
+    Return: number of subscribed or 0 otherwise
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        'User-Agent': 'Custom User Agent'  # Set your custom User-Agent here
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    from requests import get
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    r = get(url, headers, allow_redirects=False)
+
+    if r.status_code != 200:
         return 0
-if __name__ == '__main__':
-    subreddit = input("Enter the name of the subreddit: ")
-    subscribers = number_of_subscribers(subreddit)
-    print(f"The number of subscribers for /r/{subreddit} is {subscribers}")
+
+    try:
+        js = r.json()
+    except ValueError:
+        return 0
+
+    data = js.get('data')
+    if data:
+        sub_count = data.get('subscribers')
+        if sub_count:
+            return sub_count
+
+    return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,35 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
+""" Top 10 """
 
-import requests
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
+    """ computes and print a top 10 list
     Args:
-    subreddit (str): The name of the subreddit to query.
+        subreddit(str): subreddit string
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {
-        'User-Agent': 'Custom User Agent'  # Set your custom User-Agent here
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        if len(posts) == 0:
-            print(f"No posts found in /r/{subreddit}.")
-            return
-        print(f"Top 10 hot posts in /r/{subreddit}:\n")
-        for i, post in enumerate(posts[:10], start=1):
-            print(f"{i}. {post['data']['title']}")
-    else:
-        print("None")
-if __name__ == '__main__':
-    subreddit = input("Enter the name of the subreddit: ")
-    top_ten(subreddit)
+    from requests import get
+
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    r = get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        print(None)
+        return None
+
+    try:
+        js = r.json()
+    except ValueError:
+        print(None)
+        return None
+
+    try:
+        data = js.get("data")
+        children = data.get("children")
+        for child in children[:10]:
+            post = child.get("data")
+            print(post.get("title"))
+    except Exception:
+        print(None)
