655bb5aab8e66cc493328c7e637180d37d1ccb9e
Ebimary <igwemary220@gmail.com>
2023-08-08 13:10:59 -0700

    api advanvce

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,44 +1,28 @@
-#!/usr/bin/env bash
-
+#!/usr/bin/python3
+""" Exporting csv files"""
+import json
 import requests
+import sys
 
-def recurse(subreddit, hot_list=[], after=None):
-    """
-    Recursively queries the Reddit API and returns a list of titles of all hot articles for a given subreddit.
-    Args:
-    subreddit (str): The name of the subreddit to query.
-    hot_list (list): A list to store the titles of hot articles (default is an empty list).
-    after (str): A token to paginate through the results (default is None).
-    Returns:
-    list: A list containing the titles of all hot articles in the subreddit. If no results are found, returns None.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    params = {
-        'limit': 100,  # Number of items per page
-        'after': after  # Pagination token
-    }
-    headers = {
-            'User-Agent': 'Custom User Agent'  # Set your custom User-Agent here
-    }
-    response = requests.get(url, params=params, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        if not posts:
-            return hot_list
-        hot_list.extend([post['data']['title'] for post in posts])
-        after = data['data']['after']
-        if after is None:
-            return hot_list
-        return recurse(subreddit, hot_list, after)
+
+def recurse(subreddit, host_list=[], after="null"):
+    """Read reddit API and return top 10 hotspots """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    payload = {"limit": "100", "after": after}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False, params=payload)
+    if r.status_code == 200:
+        list_titles = r.json()['data']['children']
+        after = r.json()['data']['after']
+        if after is not None:
+            host_list.append(list_titles[len(host_list)]['data']['title'])
+            recurse(subreddit, host_list, after)
+        else:
+            return(host_list)
     else:
-        return None
-if __name__ == '__main__':
-    subreddit = input("Enter the name of the subreddit: ")
-    hot_titles = recurse(subreddit)
-    if hot_titles is None:
-        print("No results found for the given subreddit.")
-    else:
-        print("Hot article titles in /r/{subreddit}:\n")
-        for i, title in enumerate(hot_titles, start=1):
-            print(f"{i}. {title}")
+        return(None)
